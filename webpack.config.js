0327afaa1f36a1104c63c377ace620481ec1e910
sstruct <sstruct@gmail.com>
2016-04-25 12:20:52 +0800

    add webpack dev environment

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+var path = require('path')
+var webpack = require('webpack')
+
+module.exports = {
+  devtool: 'cheap-module-eval-source-map',
+  entry: [
+    './'
+  ],
+  output: {
+    path: path.join(__dirname, 'build'),
+    filename: 'index.js',
+    publicPath: '/'
+  },
+  devServer: {
+    inline: true,
+    port: 3000
+  },
+  plugins: [
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.HotModuleReplacementPlugin()
+  ],
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        loaders: [ 'babel' ],
+        exclude: /node_modules/,
+        include: __dirname
+      }
+    ]
+  }
+}
