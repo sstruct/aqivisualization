789de364059cf954120a660b58d73488cdaf2e7e
sstruct <sstruct@gmail.com>
2016-04-27 21:12:08 +0800

    rebuild project with yeoman

--- AqiCrawler/spiders/getMonitoringSites.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-import requests
-from bs4 import BeautifulSoup
-
-res=requests.get('http://aqicn.org/city/all/cn/#中国/m')
-soup=BeautifulSoup(res.content)
-print soup.prettify()
