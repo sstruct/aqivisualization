fe4da6c3fd6dcb3715045f38bc8362e02a358214
sstruct <sstruct@gmail.com>
2016-04-28 19:52:56 +0800

    add earthquake markers

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 import 'core-js/fn/object/assign';
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './components/Main';
+require('./components/Main');
 
 // Render the main component into the dom
-ReactDOM.render(<App />, document.getElementById('app'));
+// ReactDOM.render(<Main />, document.getElementById('app'));
