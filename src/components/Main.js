badeabca93161cfd2f733b1dc5abf5ae5c5bbbe5
sstruct <sstruct@gmail.com>
2016-05-03 20:42:49 +0800

    google-map-react

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,52 +1,11 @@
-import 'normalize.css/normalize.css';
-
+// file: main.jsx
 import React from 'react';
-import ReactDOM from 'react-dom';
-import {Gmaps, Marker, InfoWindow, Circle} from 'react-gmaps';
+import { render } from 'react-dom';
+import Layout from './Layout.js';
 
-const coords = {
-  lat: 51.5258541,
-  lng: -0.08040660000006028
-};
+import 'normalize.css/normalize.css';
+import './Main.sass';
 
-const App = React.createClass({
+const mountNode = document.getElementById('app');
 
-  onMapCreated(map) {
-    map.setOptions({
-      disableDefaultUI: true
-    });
-  },
-
-  render() {
-    return (
-      <Gmaps
-        width={'100%'}
-        height={'100vh'}
-        lat={coords.lat}
-        lng={coords.lng}
-        zoom={12}
-        loadingMessage={'AQI'}
-        params={{v: '3.exp'}}
-        onMapCreated={this.onMapCreated}>
-        <Marker
-          lat={coords.lat}
-          lng={coords.lng}
-          draggable={true}
-          onDragEnd={this.onDragEnd} />
-        <InfoWindow
-          lat={coords.lat}
-          lng={coords.lng}
-          content={'Hello, React :)'}
-          onCloseClick={this.onCloseClick} />
-        <Circle
-          lat={coords.lat}
-          lng={coords.lng}
-          radius={500}
-          onClick={this.onClick} />
-      </Gmaps>
-    );
-  }
-
-});
-
-ReactDOM.render(<App />, document.getElementById('app'));
+render(<Layout />, mountNode);
