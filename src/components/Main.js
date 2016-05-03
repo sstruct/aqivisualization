f43740fa4919b392448fba5d0149238e5202530e
sstruct <sstruct@gmail.com>
2016-05-03 14:51:56 +0800

    simple map with a text marker

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,11 +1,34 @@
-// file: main.jsx
-import React from 'react';
-import { render } from 'react-dom';
-import Layout from './Layout.js';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import GoogleMapReact from 'google-map-react'
+class SimpleMap extends React.Component {
+  state = {
+    center: [35.22, 110.48],
+    zoom: 4
+  };
 
-import 'normalize.css/normalize.css';
-import './Main.sass';
+  _onChange = ({center, zoom}) => {
+    this.setState({
+      center: center,
+      zoom: zoom
+    });
+  }
 
-const mountNode = document.getElementById('app');
+  render() {
+    return (
+       <GoogleMapReact
+        onChange={this._onChange}
+        center={this.state.center}
+        zoom={this.state.zoom}>
+        <div className="place" lat={35.22} lng={110.48}>MyPlace</div>
+      </GoogleMapReact>
+    );
+  }
+}
 
-render(<Layout />, mountNode);
+ReactDOM.render(
+  <div style={{width: '100%', height: '100vh', color: 'green'}}>
+    <SimpleMap/>
+  </div>,
+  document.getElementById('app')
+);
