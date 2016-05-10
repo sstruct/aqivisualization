bd3a0d450fa14a1b929728dce33a6b63110631fc
sstruct <sstruct@gmail.com>
2016-05-10 13:37:04 +0800

    Add getAreaData()

--- src/components/data/fakeData.js
+++ src/components/data/fakeData.js
@@ -1,43 +1,41 @@
 import Data from './2016-05-10T09:00:00Z.json'
 
-// Calculate one area's average AQI
-// let calculateAverageAQI = function(area, Data) {
-//   let monitorPoints = []
-//   let sum = 0, avg =0
-//   for(let i = 0; i<=Data.length; i++){
-//     if(Data[i].area = area){
-//       monitorPoints.push(Data[i])
-//     }
-//     sum = monitorPoints.reduce(function(a, b) { return a + b; });
-//     sum += monitorPoints[]
-//   }
-//
-//   Data.forEach((index, item) => {
-//     if(item[index].area = area){
-//       monitorPoints.push(item[index])
-//       sum += item[index].aqi
-//     }
-//     avg = sum / monitorPoints.length
-//   })
-// }
-// let hangzhouAQI = calculateAverageAQI('杭州', Data)
-
-
-
-function calculateAverageAQI(Data) {
-  let length = Data.length
+/*
+ *calculate avarage AQI
+ *@para data:array
+ */
+function calculateAverageAQI(data) {
+  let length = data.length
   let i=0, sum = 0, avg = 0
   for(i; i<length; i++){
-    sum += parseInt(Data[i].aqi, 10 )
+    // sum += parseInt(Data[i].aqi, 10 )
+    sum += parseInt(data[i].aqi, 10)
   }
   return avg = sum / length
 }
 
-let all = calculateAverageAQI(Data)
+/*
+ *get one area's data
+ *@para area:string
+ *@para data:array
+ */
+function getAreaData(area, data) {
+  let length = data.length
+  let areaData = []
+  let i = 0
+  for(; i<length; i++) {
+    if(data[i].area == area){
+      areaData.push(data[i])
+    }
+  }
+  return areaData
+}
 
-let beijingAQI = Data[0].aqi + Data[1].aqi + Data[2].aqi + Data[3].aqi
+let beijingData = getAreaData('北京', Data)
 
-const TOTAL_COUNT = beijingAQI;
+let all = calculateAverageAQI(beijingData)
+
+const TOTAL_COUNT = parseInt(all)
 
 export const zhongyuanCoords = { lat: 36.2304, lng: 111.4737 };
 
