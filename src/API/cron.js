0522912d5b51d6c9cf4554b25fbccacbcf6430e5
sstruct <sstruct@gmail.com>
2016-05-13 20:28:30 +0800

    writeFile time.json

--- src/API/cron.js
+++ src/API/cron.js
@@ -2,20 +2,14 @@
 let CronJob = require('cron').CronJob
 let getPM25cn = require('./getPM25cn')
 
-new CronJob('* 55 * * * *', function() {
+new CronJob('* 50 * * * *', function() {
   let data = new Date()
   getPM25cn.login()
   console.log('run at:', data.getSeconds(),'S',data.getMinutes(),'M')
 }, null, true, 'Asia/Shanghai')
 
-new CronJob('* 00 * * * *', function() {
+new CronJob('* 55 * * * *', function() {
   let data = new Date()
   getPM25cn.getPM25cn()
   console.log('and run at:', data.getSeconds(),'S',data.getMinutes(),'M')
 }, null, true, 'Asia/Shanghai')
-
-// new CronJob('* */50 * * * *', function() {
-//   let data = new Date()
-//   get.getPM25cn()
-//   console.log('and run at:', data.getSeconds(),'S',data.getMinutes(),'M')
-// }, null, true, 'Asia/Shanghai')
