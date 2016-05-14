569b6790889d9d2a0934a07efc386e3fbe94a93a
sstruct <sstruct@gmail.com>
2016-05-14 11:29:20 +0800

    Apply aqicn API

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,11 +1,20 @@
-// file: main.jsx
-import React from 'react';
-import { render } from 'react-dom';
-import Layout from './Layout.js';
-
 import 'normalize.css/normalize.css';
-import './Main.sass';
+import './Main.scss'
 
-const mountNode = document.getElementById('app');
-
-render(<Layout />, mountNode);
+let google = window.google
+window.onload = function(){
+  let map = new google.maps.Map(
+    document.getElementById('map'),
+    {
+      center: new google.maps.LatLng(36.2304, 111.4737),
+      mapTypeId: google.maps.MapTypeId.ROADMAP,
+      zoom: 4
+    });
+  let t = new Date().getTime();
+  let waqiMapOverlay = new google.maps.ImageMapType({
+      getTileUrl: function(coord, zoom) {
+        return 'http://tiles.aqicn.org/tiles/usepa-aqi/' + zoom + '/' + coord.x + '/' + coord.y + '.png?token=_TOKEN_ID_';
+    },
+    name: 'Air Quality', });
+  map.overlayMapTypes.insertAt(0,waqiMapOverlay);
+}
