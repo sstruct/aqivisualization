c965f36f750ebe7552396da2decbf34ab85cd397
sstruct <sstruct@gmail.com>
2016-05-14 13:57:57 +0800

    ^-^ SearchBox autocomplete

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,5 +1,6 @@
 import './Main.scss'
 import 'normalize.css/normalize.css';
+import './Header'
 
 let google = window.google
 window.onload = function(){
@@ -23,4 +24,49 @@ window.onload = function(){
     },
     name: 'AQIVisualization', });
   map.overlayMapTypes.insertAt(0,waqiMapOverlay);
+  var input = document.getElementById('cityInput')
+  var searchBox = new google.maps.places.SearchBox(input)
+  console.log(searchBox);
+  // Listen for the event fired when the user selects a prediction and retrieve
+  // more details for that place.
+  searchBox.addListener('places_changed', function() {
+    var places = searchBox.getPlaces();
+    if (places.length == 0) {
+      return;
+    }
+
+    // Clear out the old markers.
+    markers.forEach(function(marker) {
+      marker.setMap(null);
+    });
+    markers = [];
+
+    // For each place, get the icon, name and location.
+    var bounds = new google.maps.LatLngBounds();
+    places.forEach(function(place) {
+      var icon = {
+        url: place.icon,
+        size: new google.maps.Size(71, 71),
+        origin: new google.maps.Point(0, 0),
+        anchor: new google.maps.Point(17, 34),
+        scaledSize: new google.maps.Size(25, 25)
+      };
+
+      // Create a marker for each place.
+      markers.push(new google.maps.Marker({
+        map: map,
+        icon: icon,
+        title: place.name,
+        position: place.geometry.location
+      }));
+
+      if (place.geometry.viewport) {
+        // Only geocodes have viewport.
+        bounds.union(place.geometry.viewport);
+      } else {
+        bounds.extend(place.geometry.location);
+      }
+    });
+    map.fitBounds(bounds);
+  });
 }
