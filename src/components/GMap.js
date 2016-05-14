2b518e8004f5702e95154320f29b47b10309df1e
sstruct <sstruct@gmail.com>
2016-05-14 17:37:52 +0800

    SearchBox works now!

--- src/components/GMap.js
+++ src/components/GMap.js
@@ -1,10 +1,31 @@
 import './GMap.scss'
 let google = window.google
-window.onload = function(){
-  let map = new google.maps.Map(
-    document.getElementById('map'),
-    {
-      center: new google.maps.LatLng(36.2304, 111.4737),
+
+// This example uses the autocomplete feature of the Google Places API.
+// It allows the user to find all positions in a given place, within a given
+// country. It then displays markers for all the positions returned,
+// with on-click details for each hotel.
+
+let map, places;
+let autocomplete;
+let countryRestrict = {'country': 'cn'};
+
+let countries = {
+  'us': {
+    center: {lat: 37.1, lng: -95.7},
+    zoom: 3
+  },
+  'cn': {
+    center: {lat: 36.2304, lng: 111.4737},
+    zoom: 4
+  }
+};
+
+// initialize the map to center China
+window.onload = function() {
+  map = new google.maps.Map(
+    document.getElementById('map'), {
+      center: countries['cn'].center,
       mapTypeId: google.maps.MapTypeId.SATELLITE,
       zoom: 4,
       mapTypeControl: true,
@@ -14,56 +35,41 @@ window.onload = function(){
       },
       streetViewControl: false,
     });
-  let t = new Date().getTime();
+
   let waqiMapOverlay = new google.maps.ImageMapType({
       getTileUrl: function(coord, zoom) {
         return 'http://tiles.aqicn.org/tiles/usepa-aqi/' + zoom + '/' + coord.x + '/' + coord.y + '.png?token=_TOKEN_ID_';
     },
     name: 'AQIVisualization', });
+
   map.overlayMapTypes.insertAt(0,waqiMapOverlay);
-  var input = document.getElementById('cityInput')
-  var searchBox = new google.maps.places.SearchBox(input)
-  console.log(searchBox);
+
+  // Search Box
+  let input = document.getElementById('cityInput')
+  let searchBox = new google.maps.places.SearchBox(input)
+
   // Listen for the event fired when the user selects a prediction and retrieve
   // more details for that place.
   searchBox.addListener('places_changed', function() {
-    var places = searchBox.getPlaces();
+    let places = searchBox.getPlaces();
     if (places.length == 0) {
       return;
     }
+    // Create the searchbox object and associate it with the UI input control.
+    // Restrict the search to the default country, and to place type "cities".
+    let place = places[0]
+    if (place.geometry) {
+      search();
+      map.panTo(place.geometry.location);
+      map.setZoom(10);
+    } else {
+      document.getElementById('autocomplete').placeholder = 'Which City?';
+    }
+  })
+}
 
-    // Clear out the old markers.
-    markers.forEach(function(marker) {
-      marker.setMap(null);
-    });
-    markers = [];
-
-    // For each place, get the icon, name and location.
-    var bounds = new google.maps.LatLngBounds();
-    places.forEach(function(place) {
-      var icon = {
-        url: place.icon,
-        size: new google.maps.Size(71, 71),
-        origin: new google.maps.Point(0, 0),
-        anchor: new google.maps.Point(17, 34),
-        scaledSize: new google.maps.Size(25, 25)
-      };
-
-      // Create a marker for each place.
-      markers.push(new google.maps.Marker({
-        map: map,
-        icon: icon,
-        title: place.name,
-        position: place.geometry.location
-      }));
-
-      if (place.geometry.viewport) {
-        // Only geocodes have viewport.
-        bounds.union(place.geometry.viewport);
-      } else {
-        bounds.extend(place.geometry.location);
-      }
-    });
-    map.fitBounds(bounds);
-  });
+function search() {
+  let search = {
+    bounds: map.getBounds(),
+  };
 }
